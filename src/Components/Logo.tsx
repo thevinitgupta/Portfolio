5d0b93f907fc828b66a8a4fb747d230976a3a7c8
Vinit Gupta <vinitman35@gmail.com>
2022-04-05 19:50:33 +0530

    add: redirect to home from logo

--- src/Components/Logo.tsx
+++ src/Components/Logo.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 import "../Css/Logo.css"
 
 function Logo({...props}) {
     const scrolled:boolean = props.scrolled;
+    let navigate = useNavigate(); 
+  const routeChange = () =>{ 
+    let path = `/`; 
+    navigate(path);
+  }
     return (
-        <div className="Logo">
+        <div className="Logo" onClick={routeChange}>
             <div className={scrolled? "Logo-text Logo-scrolled": "Logo-text"}>
                 <span className="Logo-text-left">the</span>
                 <span className="Logo-pop">V</span>
